8d4a90d7b4b919046b483b75ee8870397fdbb8ce
Jorge <jorgeriv@gmail.com>
2013-04-20 17:49:48 -0400

    ui funcionando

--- public/js/app/create.js
+++ public/js/app/create.js
@@ -1,12 +1,8 @@
 (function (_42, window, document, undefined) {
     var View = Backbone.View.extend({
 
-        el: '#viewPort',
-
-        template: 'create',
-
         events: {
-
+            'click  a' : 'clickHandler'
         },
 
         initialize: function () {
@@ -14,29 +10,26 @@
         },
 
         render: function () {
-            this.$el.html(
-                '<div data-role="header" data-theme="a">'+
-                '<h1> CREATE BUBBLESHEET </h1>'+
-                '</div>'+
-                '<div  data-role="page" data-theme="a" id="'+this.template+'">'+
-                '<div data-role="content" data-theme="a">'+
-                '<div data-role="fieldcontain">'+
-                '<input type="text" name="name" id="name" value="" placeholder="Name" />'+
-                '</div>  '+
-                '</div>'
-            );
+            this.$el.html(Handlebars.templates[this.model.get('_id') + '.hb'](this.model.toJSON()));
             return this;
         },
         remove : function(){
             this.$el.html('');
             this.stopListening();
         },
+        clickHandler : function(e){
+            e.preventDefault();
+            var section = $(e.currentTarget).attr('href');
+            _42.router.navigate(section, {trigger : true});
+        },
+
     });
-    var Model = Backbone.Model.extend({});
+    var Model = Backbone.Model.extend({
+    });
 
     _42.create = {
         initialize: function () {
-            var model = new Model({});
+            var model = new Model({_id : 'create'});
             this.view = new View({
                 model: model
             });
