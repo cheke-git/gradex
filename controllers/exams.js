99ce3f96fa79460d9a85f073a73763baf4cfb60a
Erick Ruiz de Chavez <erickrdch@gmail.com>
2013-04-20 11:35:22 -0400

    Adding handlebars.js; Adding template loading util

--- controllers/exams.js
+++ controllers/exams.js
@@ -1,4 +1,6 @@
-var Exam = require('../models/exam.js').Exam;
+var request = require('request'),
+	qs = require('querystring'),
+	Exam = require('../models/exam.js').Exam;
 
 exports.list = function (req, res) {
 	Exam.get_all(function (err, exams) {
@@ -13,7 +15,92 @@ exports.get = function (req, res) {
 };
 
 exports.pdf = function (req, res) {
+	var url = 'http://htmltopdfapi.com/querybuilder/api.php',
+		query = {
+			url: 'http://www.google.com/',
+			orientation: 'portrait',
+			grayscale: 'false',
+			outline: 'true',
+			page_width: '210',
+			page_height: '297',
+			margin_left: '10',
+			margin_right: '10',
+			margin_top: '10',
+			margin_bottom: '10',
+			cover: '',
+			toc: 'false',
+			toc_disable_back_links: 'false',
+			toc_disable_links: 'false',
+			toc_no_dots: 'false',
+			toc_depth: '3',
+			toc_header_font_size: '15',
+			toc_header_text: '',
+			toc_l1_font_size: '12',
+			toc_l1_indentation: '0',
+			toc_l2_font_size: '10',
+			toc_l2_indentation: '20',
+			toc_l3_font_size: '8',
+			toc_l3_indentation: '40',
+			toc_l4_font_size: '6',
+			toc_l4_indentation: '60',
+			toc_l5_font_size: '4',
+			toc_l5_indentation: '80',
+			toc_l6_font_size: '2',
+			toc_l6_indentation: '100',
+			toc_l7_font_size: '0',
+			toc_l7_indentation: '120',
+			default_header: 'false',
+			header_line: 'false',
+			footer_line: 'false',
+			header_font_size: '11',
+			header_spacing: '0',
+			footer_font_size: '11',
+			footer_spacing: '0',
+			header_html: '',
+			header_left: '',
+			header_center: '',
+			header_right: '',
+			footer_html: '',
+			footer_left: '',
+			footer_center: '',
+			footer_right: '',
+			no_background: 'false',
+			stop_slow_scripts: 'true',
+			forms: 'false',
+			print_media_type: 'false',
+			disable_internal_links: 'false',
+			disable_external_links: 'false',
+			disable_javascript: 'false',
+			disable_smart_shrinking: 'false',
+			disable_pdf_compression: 'false',
+			zoom: '1',
+			copies: '1',
+			page_offset: '1',
+			outline_depth: '4',
+			dpi: '300',
+			minimum_font_size: '5',
+			title: '',
+			encoding: '',
+			user_style_sheet: '',
+			redirect_delay: '200',
+			'cookie-name': '',
+			'cookie-value': '',
+			'custom_header-name': '',
+			'custom_header-value': '',
+			username: '',
+			password: '',
+			'post-name': '',
+			'post-value': '',
+			'replace-name': '',
+			'replace-value': '',
+		};
 
+	request({
+		url: url,
+		// qs: qs.stringify(query)
+	}, function (err, response, body) {
+		res.send(body);
+	});
 };
 
 exports.add = function (req, res) {
---
+++ public/js/utils.js
@@ -0,0 +1,21 @@
+(function (_42, $) {
+	_42.loadTemplate = function (file_name, use_ajax) {
+		if (use_ajax) {
+			$.ajax({
+				url: 'js/app/templates/' + name,
+				dataType: 'text',
+				async: false,
+				error: function () {
+					return Handlebars.compile('Template not found!');
+				},
+				success: function (data) {
+					return Handlebars.compile(data);
+				}
+			});
+		} else if (Handlebars.templates[file_name]) {
+			return Handlebars.templates[file_name];
+		} else {
+			return Handlebars.compile('Error while loading template!');
+		}
+	};
+}(window._42 || {}, $));
