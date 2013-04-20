35c0d88779c217de7837d5ad951865f6b622e9b5
Jorge <jorgeriv@gmail.com>
2013-04-20 13:21:53 -0400

    add missing files

---
+++ public/js/app/home.js
@@ -0,0 +1,40 @@
+(function (_42, window, document, undefined) {
+    var View = Backbone.View.extend({
+
+        template: 'home',
+
+        events: {
+
+        },
+
+        initialize: function () {
+
+        },
+
+        render: function () {
+            this.$el.html(' <div data-role="header" data-position="inline">
+        <h1>Home</h1>
+      </div>
+      <div data-role="content" data-theme="a">
+        <a data-role="button" href="#">Create</a>
+        <a data-role="button" href="#">Grade</a>
+      </div>');
+            return this;
+        }
+    });
+    var Model = Backbone.Model.extend({});
+
+    _42.home = {
+        initialize: function () {
+            var model = new Model({});
+            this.view = new View({
+                model: model
+            });
+            return this;
+        },
+
+        remove: function() {
+           this.view.remove();
+        }
+    };
+}(window._42 = window._42 || {}, window, document));
---
+++ public/js/main.js
@@ -0,0 +1,60 @@
+(function (_42, window, document, undefined) {
+    var Router = Backbone.Router.extend({
+        routes: {
+            'home': 'home',
+            'create': 'create',
+            'grade': 'grade',
+            '*default': 'default_route'
+        },
+
+        $viewPort: null,
+
+        currentView: null,
+
+        initialize: function () {
+            this.$viewPort = $("#viewPort");
+        },
+
+        loader: function (vn) {
+            vn = vn || 'home';
+            if (this.currentView) {
+                this.currentView.remove();
+                this.currentView = null;
+            }
+            if (_42[vn]) {
+                this.currentView = _42[vn].initialize().view;
+                this.currentView.render();
+                this.$viewPort.html(this.currentView.el);
+                $.mobile.changePage();
+            } else {
+                console.log(vn + ' is not defined yet!');
+            }
+        },
+
+        home: function () {
+            // load home
+            this.loader('home');
+        },
+
+        create: function () {
+            // load create
+            this.loader('create');
+        },
+
+        grade: function () {
+            // load grade
+            this.loader('grade');
+        },
+
+        default_route: function () {
+            this.navigate('home', {
+                trigger: true
+            });
+        }
+    });
+
+    $(function () {
+        _42.router = new Router();
+        Backbone.history.start();
+    });
+}(window._42 = window._42 || {}, window, document));
